d25ee3dcb5b708a153099657d885ca6d224a9f57
sriharshaduppalli <84265640+sriharshaduppalli@users.noreply.github.com>
2023-06-21 11:08:01 +0530

    Update nlp_spacy_tokenization.py
    add spacy load

--- nlp_spacy_tokenization.py
+++ nlp_spacy_tokenization.py
@@ -1,6 +1,6 @@
 import spacy
 
-nlp = spacy.load('C:\\Users\\SDUPPALL\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python310\\site-packages\\en_core_web_sm\\en_core_web_sm-3.5.0')
+nlp = spacy.load('en_core_web_sm-3.5.0')
 doc = nlp(u'Tesla is looking at buying U.S. startup for $6 million')
 for token in doc:
     print(token.text, token.pos)
