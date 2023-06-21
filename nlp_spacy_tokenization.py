b3c289c23e2378916f0bd635010e7a6992cd8e18
sriharshaduppalli <84265640+sriharshaduppalli@users.noreply.github.com>
2023-06-21 11:13:04 +0530

    Update nlp_spacy_tokenization.py

--- nlp_spacy_tokenization.py
+++ nlp_spacy_tokenization.py
@@ -2,5 +2,13 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm-3.5.0')
 doc = nlp(u'Tesla is looking at buying U.S. startup for $6 million')
+print("\nToken.text only")
+for token in doc:
+    print(token.text)
+print("\nToken.text + Token.pos (#Part of speech)")
 for token in doc:
     print(token.text, token.pos)
+print("\nToken.text + Token.pos (#Part of speech adverb)")
+for token in doc:
+    print(token.text, token.pos_)
+
